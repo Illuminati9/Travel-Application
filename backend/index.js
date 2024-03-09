5d5dc8f76584c977ed3de5e8d234f828b6b73fea
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2024-03-09 22:47:51 +0530

    Initialised backend repository

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { connectToDatabase } = require('./config/database');
+const app = express();
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+
+
+const userRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
+const postRoutes = require('./routes/post');
+
+const fileparser = require('./config/parseFile');
+
+require('dotenv').config({ path: '.env' })
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(fileUpload({ useTempFiles: true,tempFileDir: 'tmp/',}));
+
+app.set('json spaces', 5);
+
+app.get('/api/v1/', (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the API"
+  })
+})
+
+app.use('/api/v1/auth', userRoutes);
+app.use('/api/v1/profile', profileRoutes);
+app.use('/api/v1/post', postRoutes);
+
+connectToDatabase();
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ backend/utils/constants.js
@@ -0,0 +1,12 @@
+const profileS3Url = 'uploads/profiles';
+const postS3Url = 'uploads/posts';
+
+const svgType = 'image/svg+xml';
+const svgType2 = 'image/svg';
+const jpegType = 'image/jpeg';
+const pngType = 'image/png';
+const jpgType = 'image/jpg';
+
+const allowedFileTypes = [svgType, jpegType, pngType, jpgType, svgType2];
+
+module.exports = {profileS3Url,postS3Url,svgType,jpegType,pngType,jpgType,svgType2,allowedFileTypes};
