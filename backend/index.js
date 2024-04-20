971db2f0031fbc67f5b5b8deccd580c47b1b0e50
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2024-04-20 18:07:49 +0530

    Add universal route for incorrect endpoint

--- backend/index.js
+++ backend/index.js
@@ -41,6 +41,7 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.set("json spaces", 5);
 
+
 app.get("/api/v1/", (req, res) => {
   res.status(200).json({
     message: "Welcome to the API",
@@ -54,6 +55,20 @@ app.use("/api/v1/owner", ownerRoutes);
 app.use('/api/v1/universal',universalRoutes);
 
 connectToDatabase();
+
+app.use((req, res, next) => {
+  const error = new Error('Endpoint not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+      message: 'Internal Server Error',
+      error: err.message,
+      success: false
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
