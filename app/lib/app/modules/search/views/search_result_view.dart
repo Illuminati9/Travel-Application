aa0592a54c2e6fcf1c5f48f3b613ab2e71b5b75e
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2024-04-27 19:19:22 +0530

    Complete initial search screen

--- app/lib/app/modules/search/views/search_result_view.dart
+++ app/lib/app/modules/search/views/search_result_view.dart
@@ -1,3 +1,6 @@
+import 'package:app/app/data/fake_bus_details_data.dart';
+import 'package:app/app/modules/search/widgets/bus_displaycard_widget.dart';
+import 'package:app/app/modules/universal/widgets/buttons/back_button.dart';
 import 'package:flutter/material.dart';
 
 class SearchResultView extends StatelessWidget {
@@ -6,8 +9,23 @@ class SearchResultView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(),
+      appBar: AppBar(
+        leading: const BackButtonWidget(),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text('Hello world-ksdkfjsalk'),
+          ],
+        ),
+        centerTitle: false,
+      ),
+      body: ListView.builder(
+          itemCount: busDetailsList.length,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            return BusDisplayCardWidget(busDetails: busDetailsList[index]);
+          }),
     );
   }
 }
