417983bf59d062b44ca66b3f3b1eaeb727340a3b
shishiro <dheeravathshishiro@gmail.com>
2024-06-03 20:08:39 +0530

    improved the design of the owners page

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
